2e78279cc72005bc92ede0f7f2ca661be5700591
unknown <git@github.com:Risha661/CRM.git>
2025-03-28 18:59:49 +0300

    create js for modal window

--- js/script.js
+++ js/script.js
@@ -1,6 +1,9 @@
 const btmMenu = $('.header__btn-menu');
 const btnClose = $('.header__btn-menu-close');
 const navigationList = $('.header__navigation');
+const modalBtn = $('.header__button');
+const modalWindow = $('.modal');
+const closeWindow = $('.modal__btn-close')
 
 btmMenu.click(function() {
   $(this).toggleClass('header__btn-menu-close');
@@ -12,7 +15,22 @@ btnClose.click(function() {
   btmMenu.removeClass('header__btn-menu-close');
 });
 
-// const modalBtn = $('.header__button');
-// modalBtn.click(fuction() {
-//   $('.modal').show();
-// });
+modalBtn.click(function() {
+  modalWindow.slideToggle(300);
+});
+
+closeWindow.click(function() {
+  modalWindow.slideUp(300);
+});
+
+$(document).click(function(event) {
+  if (!$(event.target).closest(navigationList).length && !$(event.target).is(btmMenu)) {
+    navigationList.slideUp(300);
+  }
+});
+
+$(document).click(function(event) {
+  if (!$(event.target).closest(modalWindow).length && !$(event.target).is(modalBtn)) {
+    modalWindow.slideUp(300);
+  }
+});
