baccfba9c06852476a29af81f98a4971ea99f051
unknown <git@github.com:Risha661/CRM.git>
2025-03-28 19:52:40 +0300

    ready btn

--- js/script.js
+++ js/script.js
@@ -5,32 +5,57 @@ const modalBtn = $('.header__button');
 const modalWindow = $('.modal');
 const closeWindow = $('.modal__btn-close')
 
-btmMenu.click(function() {
-  $(this).toggleClass('header__btn-menu-close');
-  navigationList.slideToggle(300);
-});
-
-btnClose.click(function() {
-  navigationList.slideUp(300);
-  btmMenu.removeClass('header__btn-menu-close');
-});
-
-modalBtn.click(function() {
-  modalWindow.slideToggle(300);
-});
-
-closeWindow.click(function() {
-  modalWindow.slideUp(300);
-});
-
-$(document).click(function(event) {
-  if (!$(event.target).closest(navigationList).length && !$(event.target).is(btmMenu)) {
-    navigationList.slideUp(300);
+$(document).ready(function() {
+  function checkScreenWidth() {
+    if ($(window).width() <= 590) {
+      if (navigationList.is(':visible')) {
+        $('.header__btn-border').show();
+      } else {
+        $('.header__btn-border').hide();
+      }
+    } else {
+      $('.header__btn-border').hide();
+    }
   }
-});
 
-$(document).click(function(event) {
-  if (!$(event.target).closest(modalWindow).length && !$(event.target).is(modalBtn)) {
+  checkScreenWidth();
+
+  $(window).resize(function() {
+    checkScreenWidth();
+  });
+
+  btmMenu.click(function() {
+    $(this).toggleClass('header__btn-menu-close');
+    navigationList.slideToggle(300, function() {
+      checkScreenWidth();
+    });
+  });
+
+  btnClose.click(function() {
+    navigationList.slideUp(300, function() {
+      checkScreenWidth();
+    });
+    btmMenu.removeClass('header__btn-menu-close');
+  });
+
+  modalBtn.click(function() {
+    modalWindow.slideToggle(300);
+  });
+
+  closeWindow.click(function() {
     modalWindow.slideUp(300);
-  }
+  });
+
+  $(document).click(function(event) {
+    if (!$(event.target).closest(navigationList).length && !$(event.target).is(btmMenu)) {
+      navigationList.slideUp(300, function() {
+        checkScreenWidth();
+      });
+      btmMenu.removeClass('header__btn-menu-close');
+    }
+
+    if (!$(event.target).closest(modalWindow).length && !$(event.target).is(modalBtn)) {
+      modalWindow.slideUp(300);
+    }
+  });
 });
